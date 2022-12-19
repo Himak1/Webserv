df01d46cc62d6463a225775fa073baacf892ae0c
Milan Weitenberg <mweitenb@f0r6s14.codam.nl>
2022-12-19 15:57:40 +0100

    tutorials

---
+++ inc/TcpServer.hpp
@@ -0,0 +1,39 @@
+#ifndef TCP_SERVER_HPP
+# define TCP_SERVER_HPP
+
+# include <stdio.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <stdlib.h>
+# include <string>
+
+namespace http
+{
+
+    class TcpServer
+    {
+    public:
+        TcpServer(std::string ip_address, int port);
+        ~TcpServer();
+        void                startListen();
+
+    private:
+        std::string         _ip_address;
+        int                 _port;
+        int                 _socket;
+        int                 _new_socket;
+        long                _incomingMessage;
+        struct sockaddr_in  _socketAddress;
+        unsigned int        _socketAddress_len;
+        std::string         _serverMessage;
+
+        int                 startServer();
+        void                closeServer();
+        void                acceptConnection(int &new_socket);
+        std::string         buildResponse();
+        void                sendResponse();
+    };
+
+} // namespace http
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+# include <TcpServer.hpp>
+# include <main.hpp>
+
+int main()
+{
+	using namespace http;
+
+	TcpServer server = TcpServer("0.0.0.0", 8080);
+	server.startListen();
+
+	return 0;
+}
