be8dd942771275795a1ede35370c8bb1569a3101
Justin Hille <jhille@f1r3s10.codam.nl>
2023-01-04 16:47:30 +0100

    added tokenizer cpp and hpp files

---
+++ inc/configure/tokenizer.hpp
@@ -0,0 +1,11 @@
+
+#ifndef TOKENIZER_HPP
+# define TOKENIZER_HPP
+
+# include <fstream>
+# include <vector>
+# include "Token.hpp"
+
+std::vector<Token>	tokenizer( std::ofstream file );
+
+#endif
---
+++ src/configure/tokenizer.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <string>
+#include "tokenizer.hpp"
+#include "Token.hpp"
+
+std::vector<Token>	tokenizer( std::ofstream file )
+{
+	std::vector<Token>	output;
+
+	return (output);
+}
