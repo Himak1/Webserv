a7fb6bbaf6187b4bd559980926b86979b4de87e3
Jonathan Bedaux <83814959+mo-bedo@users.noreply.github.com>
2023-01-16 14:04:06 +0100

    tcp server (WIP),

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -17,9 +17,9 @@ void	connect_to_socket()
 {
 	int pid;
 
-	pid = fork();
-	if (!pid)
-	{
+	// pid = fork();
+	// if (!pid)
+	// {
 		int 	sock = 0; 
 		long 	valread;
 		struct sockaddr_in serv_addr;
@@ -48,6 +48,7 @@ void	connect_to_socket()
 		
 		if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		{
+			perror(strerror(errno));
 			printf("\n\t\t\t######### Connection Failed client side\n");
 			// return -1;
 		}
@@ -59,49 +60,49 @@ void	connect_to_socket()
 		valread = read( sock , buffer, 1024);
 		printf("%s\n",buffer );
 		exit(0);
-	}
+	// }
 
-	{
-		int 	sock = 0; 
-		long 	valread;
-		struct sockaddr_in serv_addr;
-		static int PORT = 8001;
+	// {
+	// 	int 	sock = 0; 
+	// 	long 	valread;
+	// 	struct sockaddr_in serv_addr;
+	// 	static int PORT = 8001;
 
-		char buffer[1024] = {0};
-		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		{
-			printf("\n Socket creation error \n");
-			// return -1;
-		}
+	// 	char buffer[1024] = {0};
+	// 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	// 	{
+	// 		printf("\n Socket creation error \n");
+	// 		// return -1;
+	// 	}
 		
-		printf("port = %i\n", PORT);
+	// 	printf("port = %i\n", PORT);
 
-		memset(&serv_addr, '0', sizeof(serv_addr));
+	// 	memset(&serv_addr, '0', sizeof(serv_addr));
 		
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_port = htons(PORT);
+	// 	serv_addr.sin_family = AF_INET;
+	// 	serv_addr.sin_port = htons(PORT);
 		
-		// Convert IPv4 and IPv6 addresses from text to binary form
-		if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-		{
-			printf("\nInvalid address/ Address not supported \n");
-			// return -1;
-		}
+	// 	// Convert IPv4 and IPv6 addresses from text to binary form
+	// 	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+	// 	{
+	// 		printf("\nInvalid address/ Address not supported \n");
+	// 		// return -1;
+	// 	}
 		
-		if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-		{
-			printf("\n\t\t\t######### Connection Failed client side\n");
-			// return -1;
-		}
+	// 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	// 	{
+	// 		printf("\n\t\t\t######### Connection Failed client side\n");
+	// 		// return -1;
+	// 	}
 		
-		char *hello = "GET ";
+	// 	char *hello = "GET ";
 
-		send(sock , hello , strlen(hello) , 0 );
-		printf("Hello message sent to port %i\n", PORT);
-		valread = read( sock , buffer, 1024);
-		printf("%s\n",buffer );
-		exit(0);
-	}
+	// 	send(sock , hello , strlen(hello) , 0 );
+	// 	printf("Hello message sent to port %i\n", PORT);
+	// 	valread = read( sock , buffer, 1024);
+	// 	printf("%s\n",buffer );
+	// 	exit(0);
+	// }
 
 
 
