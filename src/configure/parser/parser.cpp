07cf0f91badb677e9f9ece1941ee88ebff8b5deb
Justin Hille <jhille@f0r4s7.codam.nl>
2023-01-18 17:40:20 +0100

    started work on implementing parseLocation() sub-functions to parse path and such

--- src/configure/parser/parser.cpp
+++ src/configure/parser/parser.cpp
@@ -44,35 +44,51 @@ Node*	parsePort()
 
 }
 
+Node*	parseLocationPath( TokenList::iterator& pos, const TokenList::iterator& end )
+{
+	Node*	newNode;
+
+}
+
 Node*	parseLocation( TokenList::iterator& pos, const TokenList::iterator& end )
 {
 	Node*	newNode;
 
-	// if ((*pos)->GetTokenType() != LOCATION))
-	// 	return (0);
-	// pos++;
-	// if ((*pos)->GetTokenType() != BRACKET_OPEN)
-	// 	return (0);
-	// pos++;
-	// if ((*pos)->GetTokenType() != BRACKET_CLOSE)
-	// 	return (0);
-	// pos++;
+	if (!accept(pos, LOCATION))
+		return (NULL);
+	newNode = new Node();
+	status = newNode.addChild(parseLocationPath(pos, end));
+	if (status == 0)
+		handle_parse_error();
+	if (token == bracket_open)
+		continue
+	while (token != bracket_close)
+	{
+		if (token == )
 
-	/*
-		if (token == location)
-			continue
-		if (token == string)
-			continue
-		if (token == bracket_open)
-			continue
-		while (token != bracket_close)
-		{
-			if (token == )
-		}
-	*/
+	}
 	return (new Node())
+	*/
 }
 
 /*
+bool	accept( TokenList::iterator& pos, int expected_token )
+{
+	if ((*pos)->getTokenType() == expected_token)
+	{
+		pos++;
+		return (true);
+	}
+	return (false);
+}
+
+bool	expect( TokenList::iterator& pos, int expected_token )
+{
+	if ((*pos)->getTokenType() == expected_token)
+	{
+		return (true);
+	}
+	return (false);
+}
 
 */
