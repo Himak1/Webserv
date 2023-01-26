9980fa0d65a4ca2a182e294035ad97423a0e522f
Jonathan Bedaux <83814959+mo-bedo@users.noreply.github.com>
2023-01-26 13:26:37 +0100

    removed unused files

--- hello.py
+++
@@ -1,9 +0,0 @@
-from socket import AF_INET, SOCK_STREAM, socket
-server_port = 8000;
-server_ip = 'localhost'
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect((server_ip, server_port))
-client_socket.send('GET codamx.nl versieVanHTTP'.encode())
-data = client_socket.recv(1024)
-print('Van server: ;', data.decode())
-client_socket.close()
