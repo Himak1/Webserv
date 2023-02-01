622c654c59c65761d78ed508e761a54374fc5581
Justin Hille <jhille@f1r2s9.codam.nl>
2023-02-01 17:00:55 +0100

    adjusted parser.hpp with the correct parameters. Need to re-imlement the functions in the cpp file and the tests

--- inc/configure/parser.hpp
+++ inc/configure/parser.hpp
@@ -9,17 +9,17 @@ class Node;
 
 Node*	parser( TokenList tList );
 
-bool	accept( TokenList::iterator& currentToken, const TokenList::iterator& ending, int expected_token );
+bool	accept( TokenStream& tokensToParse, int expected_token );
 Node*	deleteNewNode( Node* newNode );
 
-Node*	parseLocation( TokenList::iterator& currentToken, const TokenList::iterator& ending );
-Node*	parseLocationPath( TokenList::iterator& currentToken, const TokenList::iterator& ending );
-Node*   parseAlias( TokenList::iterator& currentToken, const TokenList::iterator& ending );
-Node*	parseRoot( TokenList::iterator& currentToken, const TokenList::iterator& ending );
-Node*	parseAllowedMethods( TokenList::iterator& currentToken, const TokenList::iterator& ending );
-Node*	parseCgiPass( TokenList::iterator& currentToken, const TokenList::iterator& ending );
+Node*	parseLocation( TokenStream& tokensToParse );
+Node*	parseLocationPath( TokenStream& TokensToParse );
+Node*   parseAlias( TokenStream& tokensToParse );
+Node*	parseRoot( TokenStream& tokensToParse );
+Node*	parseAllowedMethods( TokenStream& tokensToParse );
+Node*	parseCgiPass( TokenStream& tokensToParse );
 
-void    parseSingleParameter( TokenList::iterator& currentToken, const TokenList::iterator& ending );
+void    parseSingleParameter( TokenStream& tokensToParse );
 
 
 #endif
