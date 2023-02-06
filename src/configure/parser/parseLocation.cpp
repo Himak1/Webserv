1951bb6423129df410ae3fed06e63a619df50b53
Justin Hille <jhille@f1r2s15.codam.nl>
2023-02-06 12:45:56 +0100

    reimplemented parseAlias()

--- src/configure/parser/parseLocation.cpp
+++ src/configure/parser/parseLocation.cpp
@@ -5,34 +5,20 @@
 #include "TokenStream.hpp"
 #include "Node.hpp"
 
-/* bool	checkTokenSequence(int* sequence, int seqLength, TokenList::iterator currentToken, const TokenList::iterator& ending) */
-/* { */
-/* 	int	i = 0; */
+Node*	parseAlias( TokenStream& tokensToParse )
+{
+	Node*	newNode;
 
-/* 	while (i < seqLength && currentToken != ending) */
-/* 	{ */
-/* 		if (sequence[i] != (*currentToken)->getTokenType()) */
-/* 			return (0); */
-/* 		i++; */
-/* 		++currentToken; */
-/* 	} */
-/* 	return (1); */
-/* } */
-
-/* Node*   parseAlias( TokenList::iterator& currentToken, const TokenList::iterator& ending ) */
-/* { */
-/* 	Node*	newNode; */
-/* 	int		tokenSequence[] = {T_STRING, T_SEMICOLON}; */
-
-/* 	if (!accept(currentToken, ending, T_ALIAS)) */
-/* 		return (NULL); */
-/* 	newNode = new Node(N_ALIAS); */
-/* 	if (checkTokenSequence(tokenSequence, sizeof(tokenSequence), currentToken, ending)) */
-/* 		newNode->addChild(new Node(TERMINAL, (*currentToken)->getToken())); */
-/* 	++currentToken; */
-/* 	++currentToken; */
-/* 	return (newNode); */
-/* } */
+	accept(tokensToParse, T_ALIAS);
+	newNode = new Node(N_ALIAS);
+	if (expect(tokensToParse, T_STRING))
+		acceptAndCreateNewNode(tokensToParse, newNode);
+	else
+		return (deleteNewNode(newNode));
+	if (!accept(tokensToParse, T_SEMICOLON))
+		return (deleteNewNode(newNode));
+	return (newNode);
+}
 
 Node*	parseRoot( TokenStream& tokensToParse )
 {
--- test/configure_tests/parser_tests/parseLocation_tests.cpp
+++ test/configure_tests/parser_tests/parseLocation_tests.cpp
@@ -64,7 +64,6 @@ TEST(parseLocation, cgi_pass)
 	EXPECT_EQ(testInput.isEmpty(), true);
 }
 
-/*
 TEST(parseLocation, alias)
 {
 	std::list<Token*> lst = {
@@ -72,33 +71,11 @@ TEST(parseLocation, alias)
 		new Token(T_STRING, "/usr/bin/"),
 		new Token(T_SEMICOLON, ";")};
 	TokenStream testInput(lst);
+
+	Node*	output = parseAlias(testInput);
+	NodeList::const_iterator i = output->getChildrenBegin();
+	ASSERT_TRUE(output != NULL);
+	EXPECT_EQ((*i)->getNodeType(), TERMINAL);
+	EXPECT_EQ((*i)->getTerminal(), "/usr/bin/");
 }
-*/
-
-/* TEST(parseLocation, alias) */
-/* { */
-/* 	// setup */
-/* 	TokenList	tList; */
-/* 	tList.push_back(new Token(T_ALIAS, "alias")); */
-/* 	tList.push_back(new Token(T_STRING, "/usr/bin/")); */
-/* 	tList.push_back(new Token(T_SEMICOLON, ";")); */
-/* 	TokenList::iterator iter = tList.begin(); */
-/* 	TokenList::iterator end = tList.end(); */
-
-/* 	Node*	output; */
-/* 	output = parseAlias(iter, end); */
-/* 	ASSERT_TRUE(output != nullptr); */
-/* 	if (output) */
-/* 		delete output; */
-/* 	tList.clear(); */
-
-/* 	TokenList	lst; */
-/* 	lst.push_back(new Token(T_ALIAS, "alias")); */
-/* 	lst.push_back(new Token(T_STRING, "/usr/bin/")); */
-/* 	lst.push_back(new Token(T_STRING, "garbage")); */
-/* 	lst.push_back(new Token(T_SEMICOLON, ";")); */
-/* 	iter = lst.begin(); */
-/* 	end = lst.end(); */
-/* 	output = parseAlias(iter, end); */
-/* } */
 
